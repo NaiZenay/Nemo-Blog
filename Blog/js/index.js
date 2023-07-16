ad48f3e326943261bc69ca7616fe247e79ff7952
NaiZenay <115850010+NaiZenay@users.noreply.github.com>
2023-07-16 14:04:37 -0600

    Subiendo base del proyecto (Blog)

---
+++ Blog/js/index.js
@@ -0,0 +1,6 @@
+const navBar=document.querySelector('#navbarNav');
+const navHome=document.querySelector('#nav_home');
+const navPost=document.querySelector('#nav_post');
+const navComment=document.querySelector('#nav_comentarios');
+
+
