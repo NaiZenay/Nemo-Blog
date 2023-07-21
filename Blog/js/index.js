9a229c8891612b98e0b1ade4c437a48371f95f12
NaiZenay <115850010+NaiZenay@users.noreply.github.com>
2023-07-21 12:19:04 -0600

    Update index.js
    Arreglando responsive

--- Blog/js/index.js
+++ Blog/js/index.js
@@ -1,6 +1,13 @@
 
 // Responsive el cuerpo pricipal del post segun el tamaño de la pantalla 
 mainPost=document.querySelector('#main-post');
+if(screen.width <= 480 ){
+    mainPost.classList.remove('col-9') ;
+    mainPost.classList.add('col-12');   
+}else{
+    mainPost.classList.remove('col-12') ;
+    mainPost.classList.add('col-9'); 
+}
 window.addEventListener('resize',()=>{
     if(screen.width <= 480 ){
         mainPost.classList.remove('col-9') ;
