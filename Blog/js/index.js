150894b65f59caa75920d2e0d166e719cfdfb56e
NaiZenay <115850010+NaiZenay@users.noreply.github.com>
2023-07-21 12:21:55 -0600

    Update index.js
    .

--- Blog/js/index.js
+++ Blog/js/index.js
@@ -1,13 +1,16 @@
-
 // Responsive el cuerpo pricipal del post segun el tamaño de la pantalla 
-mainPost=document.querySelector('#main-post');
-if(screen.width <= 480 ){
-    mainPost.classList.remove('col-9') ;
-    mainPost.classList.add('col-12');   
-}else{
-    mainPost.classList.remove('col-12') ;
-    mainPost.classList.add('col-9'); 
-}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    mainPost=document.querySelector('#main-post');
+    if(screen.width <= 480 ){
+        mainPost.classList.remove('col-9') ;
+        mainPost.classList.add('col-12');   
+    }else{
+        mainPost.classList.remove('col-12') ;
+        mainPost.classList.add('col-9'); 
+    }
+})
+
 window.addEventListener('resize',()=>{
     if(screen.width <= 480 ){
         mainPost.classList.remove('col-9') ;
