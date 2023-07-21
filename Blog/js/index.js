3eaa8b32606e85182d5aaac52fd7a51ac838a376
NaiZenay <115850010+NaiZenay@users.noreply.github.com>
2023-07-21 12:14:07 -0600

    eliminado log y acomodadno modal de info en el post

--- Blog/js/index.js
+++ Blog/js/index.js
@@ -3,11 +3,9 @@
 mainPost=document.querySelector('#main-post');
 window.addEventListener('resize',()=>{
     if(screen.width <= 480 ){
-        console.log('funciono');
         mainPost.classList.remove('col-9') ;
         mainPost.classList.add('col-12');   
     }else{
-        console.log('funciono -');
         mainPost.classList.remove('col-12') ;
         mainPost.classList.add('col-9'); 
     }
