a28500060cf9fd213703917eafcc15c286e46738
NaiZenay <115850010+NaiZenay@users.noreply.github.com>
2023-07-21 12:01:58 -0600

    Responsive de index con js

--- Blog/js/index.js
+++ Blog/js/index.js
@@ -1,6 +1,18 @@
-const navBar=document.querySelector('#navbarNav');
-const navHome=document.querySelector('#nav_home');
-const navPost=document.querySelector('#nav_post');
-const navComment=document.querySelector('#nav_comentarios');
+
+// Responsive el cuerpo pricipal del post segun el tamaño de la pantalla 
+mainPost=document.querySelector('#main-post');
+window.addEventListener('resize',()=>{
+    if(screen.width <= 480 ){
+        console.log('funciono');
+        mainPost.classList.remove('col-9') ;
+        mainPost.classList.add('col-12');   
+    }else{
+        console.log('funciono -');
+        mainPost.classList.remove('col-12') ;
+        mainPost.classList.add('col-9'); 
+    }
+})
+
+    
 
 
