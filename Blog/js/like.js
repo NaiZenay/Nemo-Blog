99654c9ee0e2fe46f5c7146c09b98606439ece0e
NaiZenay <115850010+NaiZenay@users.noreply.github.com>
2023-10-10 11:46:04 -0600

    Arreglos generales

--- Blog/js/like.js
+++ Blog/js/like.js
@@ -15,3 +15,20 @@ like_btn.addEventListener('click', () => {
     }
 });
 
+const comment_like_container = document.querySelector('#comment_like_container');
+const comment_like_btn = document.querySelector('#comment_like_btn');
+const comment_like_icon = document.querySelector('#comment_like_icon');
+const comment_contador_likes = document.querySelector('#comment_contador_likes')
+
+
+comment_like_btn.addEventListener('click', () => {
+    if(comment_like_icon.classList.contains('fa-regular')){
+        comment_like_icon.classList.remove('fa-regular');
+        comment_like_icon.classList.add('fa-solid');
+        return;
+    }else if(comment_like_icon.classList.contains('fa-solid')){
+        comment_like_icon.classList.remove('fa-solid');
+        comment_like_icon.classList.add('fa-regular');
+    }
+});
+
