eea0c85d2f6446532992ad59d7102e7736c4411d
NaiZenay <115850010+NaiZenay@users.noreply.github.com>
2023-10-13 16:55:38 -0600

    Correcion en js de buscador

--- Blog/js/buscador.js
+++ Blog/js/buscador.js
@@ -4,16 +4,15 @@ const searchByTitlePost = document.querySelector('#searchByTitlePost');
 const searchByUser = document.querySelector('#searchByUser');
 
 searchByTags.addEventListener('click', ()=>{
-    searchBar.placeholder='Busca por tags ej: Programacion Web';
     searchBar.disabled=false;
-
+    searchBar.placeholder='Busca por tags ej: Programacion Web';
 });
 searchByUser.addEventListener('click', ()=>{
-    searchBar.placeholder='Busca por nombre de Usuario o Apodo ej: Nemo';
     searchBar.disabled=false;
-
+    searchBar.placeholder='Busca por nombre de Usuario o Apodo ej: Nemo';
 });
 searchByTitlePost.addEventListener('click', ()=>{
-    searchBar.placeholder='Busca por Titulo de Post ej:Ver-Gas';
     searchBar.disabled=false;
+    searchBar.placeholder='Busca por Titulo de Post ej:Ver-Gas';
+
 });
