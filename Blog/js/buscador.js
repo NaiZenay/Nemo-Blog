f1b9470dd6eb29734abfe6f56e594536e859ef1c
NaiZenay <115850010+NaiZenay@users.noreply.github.com>
2023-10-15 01:01:22 -0600

    prevent Defalut y mejora de dropdown

--- Blog/js/buscador.js
+++ Blog/js/buscador.js
@@ -3,16 +3,13 @@ const searchByTags = document.querySelector('#searchByTag');
 const searchByTitlePost = document.querySelector('#searchByTitlePost');
 const searchByUser = document.querySelector('#searchByUser');
 
-searchByTags.addEventListener('click', ()=>{
-    searchBar.disabled=false;
-    searchBar.placeholder='Busca por tags ej: Programacion Web';
-});
-searchByUser.addEventListener('click', ()=>{
-    searchBar.disabled=false;
-    searchBar.placeholder='Busca por nombre de Usuario o Apodo ej: Nemo';
-});
-searchByTitlePost.addEventListener('click', ()=>{
-    searchBar.disabled=false;
-    searchBar.placeholder='Busca por Titulo de Post ej:Ver-Gas';
 
-});
+searchByTags.addEventListener('click', e => cambioBuscador(e, 'Busca por tags ej: Programacion Web'));
+searchByUser.addEventListener('click', e => cambioBuscador(e, 'Busca por nombre de Usuario o Apodo ej: Nemo'));
+searchByTitlePost.addEventListener('click', e => cambioBuscador(e, 'Busca por Titulo de Post ej:Ver-Gas'));
+
+function cambioBuscador(e, mensaje) {
+    e.preventDefault();
+    searchBar.disabled = false;
+    searchBar.placeholder = mensaje;
+}
