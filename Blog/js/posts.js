259420a0ccc4c79a47a45993c0e0b879353d0524
Ian Samuel Yañez Sanchez <a23310131@ceti.mx>
2023-11-03 09:14:15 -0600

    Select de tags

--- Blog/js/posts.js
+++ Blog/js/posts.js
@@ -2,15 +2,19 @@
 let creationDate;
 const author = document.querySelector('#author');
 
-const back_edit_Btn = document.querySelector('#back_edit');
 
 // inputs creacion de post 
 const title_post_Input = document.querySelector('#title_post');
 const body_post_Input = document.querySelector('#body_post');
 const foot_post_Input = document.querySelector('#foot_post');
+const tags_Select=document.querySelector('#tags_Select');
 
+//Buttons
 const cancel_Btn = document.querySelector('#cancel');
 const preview_Btn = document.querySelector('#preview');
+const back_edit_Btn = document.querySelector('#back_edit');
+const post = document.querySelector('#post');
+
 
 // Preview post 
 const fecha_publicacion_preview = document.querySelector('#fecha_publicacion');
@@ -18,15 +22,14 @@ const titulo_publicacion_preview = document.querySelector('#titulo_publicacion')
 
 const cuerpo_publicacion_preview = document.querySelector('#cuerpo_publicacion');
 const contador_char = document.querySelector('#contador_char');
-const body_post_alert=document.querySelector('#body_post_alert');
+const body_post_alert = document.querySelector('#body_post_alert');
 
 const pie_publicacion_preview = document.querySelector('#pie_publicacion');
 const autor_post_preview = document.querySelector('#autor_post');
 const autor_post_repsonsive = document.querySelector('#autor_post_repsonsive');
 
-const post = document.querySelector('#post');
 
-const patronURL = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+// const patronURL = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
 
 const postData = {
     titulo: null,
@@ -35,12 +38,14 @@ const postData = {
     fecha: null,
     autor: author.value
 }
+const tags = ['Programacion', 'Diseño', 'Musica', 'Web', 'Emprendimiento','Crear Tag'];
 
 //Editar formato TO-DO
 crearPostModalBtn.addEventListener('click', () => {
     post.disabled = true;
     creationDate = moment().locale('es').format('YYYY-MM-DD, h:mm');
     postData.fecha = creationDate;
+    llenarTagsSelect(tags);
 });
 
 title_post_Input.addEventListener('change', () => {
@@ -50,12 +55,12 @@ title_post_Input.addEventListener('change', () => {
 body_post_Input.addEventListener('input', (evt) => {
     contador_char.textContent = evt.target.value.length;
     postData.cuerpo = body_post_Input.value;
-    if(evt.target.value.length === 420 || evt.target.value.length >= 30 ){
-        body_post_alert.hidden=true;
+    if (evt.target.value.length === 420 || evt.target.value.length >= 30) {
+        body_post_alert.hidden = true;
         contador_char.classList.remove('text-danger');
         contador_char.classList.add('textoV2');
-    }else if(evt.target.value.length < 30){
-        body_post_alert.hidden=false;
+    } else if (evt.target.value.length < 30) {
+        body_post_alert.hidden = false;
     }
 });
 
@@ -87,7 +92,6 @@ function llenarPreview() {
     autor_post_repsonsive.textContent = postData.autor;
 }
 
-
 function validarCamposPost() {
     if (postData.titulo.length != 0 &&
         postData.cuerpo.length > 30) {
@@ -106,6 +110,15 @@ function resetPostData() {
     body_post_Input.value = null;
     foot_post_Input.value = null;
 }
+function llenarTagsSelect(tags=[]) {
+    tags.forEach(option => {
+        const tag = document.createElement('option');
+        tag.value = option;
+        tag.textContent = option ;
+        tags_Select.appendChild(tag);    
+    });
+}
+
 
 
 
