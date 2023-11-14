505aa23982b283458674af587fd008115b6c667b
Ian Samuel Yañez Sanchez <a23310131@ceti.mx>
2023-11-13 23:55:57 -0600

    Vista previa tags

--- Blog/js/posts.js
+++ Blog/js/posts.js
@@ -7,7 +7,8 @@ const author = document.querySelector('#author');
 const title_post_Input = document.querySelector('#title_post');
 const body_post_Input = document.querySelector('#body_post');
 const foot_post_Input = document.querySelector('#foot_post');
-const tags_Select=document.querySelector('#tags_Select');
+const tags_Select = document.querySelector('#tags_Select');
+const tags_selected = document.querySelector('#tags_selected');
 
 //Buttons
 const cancel_Btn = document.querySelector('#cancel');
@@ -28,6 +29,10 @@ const pie_publicacion_preview = document.querySelector('#pie_publicacion');
 const autor_post_preview = document.querySelector('#autor_post');
 const autor_post_repsonsive = document.querySelector('#autor_post_repsonsive');
 
+const tags_publicacion = document.querySelector('#tags_publicacion');
+const tags_publicacion_Responsive = document.querySelector('#tags_publicacion_Responsive');
+
+
 
 // const patronURL = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
 
@@ -38,7 +43,9 @@ const postData = {
     fecha: null,
     autor: author.value
 }
-const tags = ['Programacion', 'Diseño', 'Musica', 'Web', 'Emprendimiento','Crear Tag'];
+const tags = ['Programacion', 'Diseño', 'Musica', 'Web', 'Emprendimiento'];
+let tags_Selected = [];
+
 
 //Editar formato TO-DO
 crearPostModalBtn.addEventListener('click', () => {
@@ -47,11 +54,9 @@ crearPostModalBtn.addEventListener('click', () => {
     postData.fecha = creationDate;
     llenarTagsSelect(tags);
 });
-
 title_post_Input.addEventListener('change', () => {
     postData.titulo = title_post_Input.value;
 });
-
 body_post_Input.addEventListener('input', (evt) => {
     contador_char.textContent = evt.target.value.length;
     postData.cuerpo = body_post_Input.value;
@@ -63,25 +68,24 @@ body_post_Input.addEventListener('input', (evt) => {
         body_post_alert.hidden = false;
     }
 });
-
 foot_post_Input.addEventListener('change', () => {
     postData.pie = foot_post_Input.value;
 
 });
-
 preview_Btn.addEventListener('click', () => {
     post.disabled = true;
     llenarPreview();
     validarCamposPost();
-})
-
+});
 back_edit_Btn.addEventListener('click', () => {
     post.disabled = true;
     validarCamposPost();
-})
+});
 cancel_Btn.addEventListener('click', () => {
     resetPostData();
-})
+});
+
+tags_Select.addEventListener('change', mostrarTags);
 
 function llenarPreview() {
     fecha_publicacion_preview.textContent = postData.fecha;
@@ -90,6 +94,7 @@ function llenarPreview() {
     pie_publicacion_preview.textContent = postData.pie;
     autor_post_preview.textContent = postData.autor;
     autor_post_repsonsive.textContent = postData.autor;
+    llenarTagsPreview();
 }
 
 function validarCamposPost() {
@@ -110,15 +115,52 @@ function resetPostData() {
     body_post_Input.value = null;
     foot_post_Input.value = null;
 }
-function llenarTagsSelect(tags=[]) {
+function llenarTagsSelect(tags = []) {
     tags.forEach(option => {
         const tag = document.createElement('option');
         tag.value = option;
-        tag.textContent = option ;
-        tags_Select.appendChild(tag);    
+        tag.textContent = option;
+        tags_Select.appendChild(tag);
     });
 }
 
+function mostrarTags(e) {
+    const tag = document.createElement('LI');
+    tags_Selected.push(e.target.value);
+    tag.textContent = e.target.value;
+    tags_selected.appendChild(tag);
+    const btnClose = document.createElement('BUTTON');
+    btnClose.classList.add('btn');
+    btnClose.innerHTML = `<i class="fa-solid fa-xmark icons"></i>`;
+    tag.appendChild(btnClose);
+
+    btnClose.addEventListener('click', () => {
+        tag.remove()
+        tags_Selected.pop(tag);
+    })
+
+}
+
+function llenarTagsPreview() {
+    limpiarHTML(tags_publicacion);
+    limpiarHTML(tags_publicacion_Responsive);
+    tags_Selected.forEach(tag => {
+        const li_tag = document.createElement('LI');
+        li_tag.classList.add('dropdown-item','primario');
+        li_tag.innerHTML=`
+        <i class="fa-solid fa-tag icons"></i>
+        ${tag}
+        `
+        tags_publicacion.appendChild(li_tag);
+        tags_publicacion_Responsive.appendChild(li_tag);
+    })
+}
+
+function limpiarHTML(selector){
+    while(selector.firstChild){
+        selector.removeChild(selector.firstChild);
+    }
+}
 
 
 
