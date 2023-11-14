3bd254bdb9aeb3edf873f9cad6c4ebb41f84c7b8
Ian Samuel Yañez Sanchez <a23310131@ceti.mx>
2023-11-13 20:59:37 -0600

    V2 buscador

--- Blog/js/buscador.js
+++ Blog/js/buscador.js
@@ -1,15 +1,28 @@
+const searchForm = document.querySelector('#searchForm');
 const searchBar = document.querySelector('#searchBar');
 const searchByTags = document.querySelector('#searchByTag');
 const searchByTitlePost = document.querySelector('#searchByTitlePost');
 const searchByUser = document.querySelector('#searchByUser');
+const buscar_Btn = document.querySelector('#buscar');
+
+const img_preResult = document.querySelector('#img_preResult');
+const post_resultados = document.querySelector('#post_result');
 
 
-searchByTags.addEventListener('click', e => cambioBuscador(e, 'Busca por tags ej: Programacion Web'));
-searchByUser.addEventListener('click', e => cambioBuscador(e, 'Busca por nombre de Usuario o Apodo ej: Nemo'));
-searchByTitlePost.addEventListener('click', e => cambioBuscador(e, 'Busca por Titulo de Post ej:Ver-Gas'));
+searchByTags.addEventListener('click', e => cambioBuscador(e, 'Busca tags ej: Programacion'));
+searchByUser.addEventListener('click', e => cambioBuscador(e, 'Busca Usuario o Apodo ej: Nemo'));
+searchByTitlePost.addEventListener('click', e => cambioBuscador(e, 'Busca por Titulo ej:Ver-Gas'));
+
+buscar_Btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    img_preResult.hidden=true;
+    post_resultados.hidden=false;
+});
 
 function cambioBuscador(e, mensaje) {
     e.preventDefault();
     searchBar.disabled = false;
+    searchForm.hidden = false;
+    searchForm.reset();
     searchBar.placeholder = mensaje;
 }
